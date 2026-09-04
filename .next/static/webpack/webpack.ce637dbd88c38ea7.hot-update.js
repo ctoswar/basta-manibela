a2575c6698e13c3fee73ee39ebfbe61332f2654e
ctos <ctos123game@gmail.com>
2026-09-05 01:01:44 +0800

    feat: add hot update files for webpack and layout, and create type definitions for admin page

--- .next/static/webpack/webpack.ce637dbd88c38ea7.hot-update.js
+++ .next/static/webpack/webpack.ce637dbd88c38ea7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7714b6bbe4474a3c"; }
+/******/ 	__webpack_require__.h = function() { return "11330ff0a02536de"; }
 /******/ }();
 /******/ 
 /******/ }
